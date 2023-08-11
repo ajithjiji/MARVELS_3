a8e75e31fc7dba2b64bbe0d63863645e5f6f87c4
ajithjiji <122249224+ajithjiji@users.noreply.github.com>
2023-08-11 12:45:12 +0530

    Update Marvels.py

--- Marvels.py
+++ Marvels.py
@@ -4,7 +4,7 @@
 # In[1]:
 #pip install streamlit
 #pip install scikit-learn-intelex
-get_ipython().system('pip install scikit-learn-intelex')
+#get_ipython().system('pip install scikit-learn-intelex')
 from sklearnex import patch_sklearn
 patch_sklearn()
 
